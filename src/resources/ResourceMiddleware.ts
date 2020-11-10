7d7615c756f34fb0c79517e7609c943750787e87
Breno Salles <git@brenosalles.com>
2020-11-10 23:38:26 +0000

    feat: Added ability to decorate/proxy a Resource
    To use this, simply extend this class and `override` it's
    implementation recalling the equivelent method of `super` when
    retrieving the original behaviour.
    
    Not only a basic Resource can be decorated/proxied. A decorator can also
    be decorated by other decorators (same is true for a proxy).
    
    The main difference between a decorator and a proxy is that a proxy can
    break the behavioural chain whereas the decorator doesn't, but all this
    under the same API. The way the end user uses is what will tell if it's
    one or another.
    
    Based on Decorator and Proxy Pattern.
    https://refactoring.guru/design-patterns/decorator
    https://refactoring.guru/design-patterns/proxy

---
+++ src/resources/ResourceMiddleware.ts
@@ -0,0 +1,48 @@
+import { Request } from "../../deps.ts";
+import { IResource } from "./IResource.ts";
+
+class ResourceMiddleware implements IResource {
+  private wrappe: IResource;
+
+  public constructor(wrappe: IResource) {
+    this.wrappe = wrappe;
+  }
+
+  public CONNECT(request: Request) {
+    return this.wrappe.CONNECT(request);
+  }
+
+  public DELETE(request: Request) {
+    return this.wrappe.DELETE(request);
+  }
+
+  public GET(request: Request) {
+    return this.wrappe.GET(request);
+  }
+
+  public HEAD(request: Request) {
+    return this.wrappe.HEAD(request);
+  }
+
+  public OPTIONS(request: Request) {
+    return this.wrappe.OPTIONS(request);
+  }
+
+  public PATCH(request: Request) {
+    return this.wrappe.PATCH(request);
+  }
+
+  public POST(request: Request) {
+    return this.wrappe.POST(request);
+  }
+
+  public PUT(request: Request) {
+    return this.wrappe.PUT(request);
+  }
+
+  public TRACE(request: Request) {
+    return this.wrappe.TRACE(request);
+  }
+}
+
+export { ResourceMiddleware };
