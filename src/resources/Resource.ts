ec908f881cd89bc22b7ce5b2937db7f34faf3691
Breno Salles <git@brenosalles.com>
2020-11-10 23:01:01 +0000

    feat: Added default abstraction of a Resource
    This provides the default implementation of a Resource as it implements
    the IResource interface.
    
    When a user wants to create a Resource, it simply `extends` this
    implementation and overrides whatever method it wants to override given
    by the interface.

---
+++ src/resources/Resource.ts
@@ -0,0 +1,72 @@
+import { Request } from "../../deps.ts";
+import { Response } from "../../deps.ts";
+import { IResource } from "./IResource.ts";
+
+class Resource implements IResource {
+  public uri: string[] = [];
+
+  public async CONNECT(request: Request) {
+    const response: Response = {
+      status: 404,
+    };
+    return response;
+  }
+
+  public async DELETE(request: Request) {
+    const response: Response = {
+      status: 404,
+    };
+    return response;
+  }
+
+  public async GET(request: Request) {
+    const response: Response = {
+      status: 404,
+    };
+    return response;
+  }
+
+  public async HEAD(request: Request) {
+    const response: Response = {
+      status: 404,
+    };
+    return response;
+  }
+
+  public async OPTIONS(request: Request) {
+    const response: Response = {
+      status: 404,
+    };
+    return response;
+  }
+
+  public async PATCH(request: Request) {
+    const response: Response = {
+      status: 404,
+    };
+    return response;
+  }
+
+  public async POST(request: Request) {
+    const response: Response = {
+      status: 404,
+    };
+    return response;
+  }
+
+  public async PUT(request: Request) {
+    const response: Response = {
+      status: 404,
+    };
+    return response;
+  }
+
+  public async TRACE(request: Request) {
+    const response: Response = {
+      status: 404,
+    };
+    return response;
+  }
+}
+
+export { Resource };
